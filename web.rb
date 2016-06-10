655bd396322474d09c10dce8ba774bd34d0ea3d4
hernanofx <hernanofx@hotmail.com>
2016-06-10 16:07:41 -0300

    ruby program

--- web.rb
+++ web.rb
@@ -1,12 +1,11 @@
 require 'sinatra'
 
 get '/' do
-  "7 horas para configurar esta mierda!"
 
-a = 10
-b = 11
+print "Cual es tu nombre?"
+tu_nombre = gets.chomp
 
-  print "Cual es tu nombre?"
+
 
 
 end
