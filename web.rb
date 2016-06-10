6d3515f3232d91e4a6fd5c82a20d2a42f0d01e36
hernanofx <hernanofx@hotmail.com>
2016-06-10 15:56:37 -0300

    ruby program

--- web.rb
+++ web.rb
@@ -2,4 +2,15 @@ require 'sinatra'
 
 get '/' do
   "7 horas para configurar esta mierda!"
+
+a = 10
+b = 11
+
+  if a < b
+    print "a es menor que b!"
+  elsif b > a
+    print "b es menor que a!"
+  else
+    print "b es igual a a!"
+  end
 end
