9e8ca5df302825f644caae45d95aec3c81d87a00
hernanofx <hernanofx@hotmail.com>
2016-07-05 16:27:03 -0300

    presupuesto ruby

---
+++ views/presupuesto.rb
@@ -0,0 +1,16 @@
+require 'rubygems'
+require 'sinatra'
+
+
+class presupuesto
+presupuesto = 0
+instpro = 150
+instso = 200
+instmemo = 100
+placas = 150
+fuente = 150
+retiro = 0
+entrega = 100
+copias = 80
+
+puts "#{presupuesto} + {instpro} + {instso} + {instmemo} + {placas} + {fuente} + {retiro} + {entrega} + {copias}"
