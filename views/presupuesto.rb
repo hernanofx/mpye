7de4b70c26f934a47e6844ab6eb44157eea4ba2e
hernanofx <hernanofx@hotmail.com>
2016-07-05 16:58:50 -0300

    presupuesto ruby

---
+++ views/presupuesto.rb
@@ -0,0 +1,13 @@
+
+class presupuesto
+presupuesto = 0
+instpro = 150
+instso = 200
+instmemo = 100
+placas = 150
+fuente = 150
+retiro = 0
+entrega = 100
+copias = 80
+
+print presupuesto + instpro + instso + instmemo + placas + fuente + retiro + entrega + copias
